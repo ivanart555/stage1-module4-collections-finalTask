31eeff53a6172d4568644a46bb17971a3633f7ca
Ivan Artsiushyn <ivanart555@gmail.com>
2023-07-26 22:31:47 +0300

    Solve tasks

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,20 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new ArrayList<>();
+
+        for (Map.Entry<String, Set<String>> e : projects.entrySet()) {
+            if (e.getValue().contains(developer)) result.add(e.getKey());
+        }
+
+        result.sort(new ProjectsComparator());
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,26 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> result = new HashMap<>();
+
+        for (Map.Entry<String, Integer> e : sourceMap.entrySet()) {
+            int wordLength = e.getKey().length();
+
+            if (result.containsKey(wordLength)) {
+                result.get(wordLength).add(e.getKey());
+            } else {
+                Set<String> wordsSet = new HashSet<>();
+                wordsSet.add(e.getKey());
+                result.put(wordLength, wordsSet);
+            }
+        }
+
+        return result;
     }
 }
